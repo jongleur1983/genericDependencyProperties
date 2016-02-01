65eae23ebc6d923479fa43799a6ea983097d20a6
Peter Wendorff <jongleur1983@gmx.de>
2016-02-02 00:20:43 +0100

    add a simple example app (not fully functional yet, but some converters inside, and building.

---
+++ GenericDependencyProperties.Examples/SampleUserControl/SampleUserControl.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GenericDependencyProperties;
+using GenericDependencyProperties.GenericMetadata;
+
+namespace GenericDependencyProperties.Examples
+{
+    /// <summary>
+    /// Interaktionslogik für SampleUserControl.xaml
+    /// </summary>
+    public partial class SampleUserControl : UserControl
+    {
+        public static DependencyProperty SatelliteRadiusProperty = GenericDependencyProperty.Register(
+            "SatelliteRadius",
+            new GenericPropertyMetadata<double, SampleUserControl>(20)
+            );
+
+        public double SatelliteRadius
+        {
+            get
+            {
+                return (double) this.GetValue(SatelliteRadiusProperty); // TODO: can we get rid of the cast as well? not yet as the dependencyProperty itself is untyped
+            }
+            set
+            {
+                this.SetValue(SatelliteRadiusProperty, value);
+            }
+        }
+
+        //public static DependencyProperty HeadlineProperty = GenericDependencyProperties.Register(
+        //    "Headline", 
+        //    new GenericPropertyMetadata<string, SampleUserControl>("defaultHeadline"));
+
+        public SampleUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
