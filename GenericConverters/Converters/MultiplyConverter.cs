f92ce27df49c855410694f09a0661c180a11f17d
Peter Wendorff <jongleur1983@gmx.de>
2016-02-02 00:17:46 +0100

    Add a MultiplyConverter as another sample (nothing more interesting inside, but useful for upcoming demo)

---
+++ GenericConverters/Converters/MultiplyConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace GenericConverters.Converters
+{
+    public class MultiplyConverter : GenericValueConverter<double, double, double>
+    {
+        public override double Convert(double from, double parameter, CultureInfo culture)
+        {
+            var factor = parameter;
+            return from * factor;
+        }
+
+        public override double ConvertBack(double value, double parameter, CultureInfo culture)
+        {
+            var factor = parameter;
+            return value / factor;
+        }
+    }
+}
