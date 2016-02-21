c0334440627198d529b926ab18c9e73cf7cec94b
Peter Wendorff <jongleur1983@gmx.de>
2016-02-21 19:25:40 +0100

    code cleanup

--- GenericDependencyProperties.Examples/SampleUserControl/SampleUserControl.xaml.cs
+++ GenericDependencyProperties.Examples/SampleUserControl/SampleUserControl.xaml.cs
@@ -1,18 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using GenericDependencyProperties;
 using GenericDependencyProperties.GenericMetadata;
 
 namespace GenericDependencyProperties.Examples
@@ -23,7 +10,7 @@ namespace GenericDependencyProperties.Examples
     public partial class SampleUserControl : UserControl
     {
         public static DependencyProperty SatelliteRadiusProperty = GenericDependencyProperty.Register(
-            "SatelliteRadius",
+            nameof(SatelliteRadius),
             new GenericPropertyMetadata<double, SampleUserControl>(20)
             );
 
