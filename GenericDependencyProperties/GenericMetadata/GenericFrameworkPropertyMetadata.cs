66f494d1bf63821d0082d08926555c4d4274df8c
Peter Wendorff <peter.wendorff@gmx.de>
2016-08-06 07:51:34 +0200

    #15: implement constructors for GenericUIPropertyMetadata and GenericFrameworkPropertyMetadata.

--- GenericDependencyProperties/GenericMetadata/GenericFrameworkPropertyMetadata.cs
+++ GenericDependencyProperties/GenericMetadata/GenericFrameworkPropertyMetadata.cs
@@ -1,10 +1,44 @@
 ﻿using System.Windows;
+using GenericDependencyProperties.GenericCallback;
 
 namespace GenericDependencyProperties.GenericMetadata
 {
     public class GenericFrameworkPropertyMetadata<TProperty, TOwner> : GenericUIPropertyMetadata<TProperty, TOwner> 
         where TOwner : FrameworkElement
     {
+        public GenericFrameworkPropertyMetadata()
+            : base()
+        {
+        }
+
+        public GenericFrameworkPropertyMetadata(
+            GenericPropertyChangedCallback<TOwner, TProperty> propertyChangedCallback)
+            : base(propertyChangedCallback)
+        {
+        }
+
+        public GenericFrameworkPropertyMetadata(
+            GenericPropertyChangedCallback<TOwner, TProperty> propertyChangedCallback,
+            GenericCoerceValueCallback<TOwner, TProperty> coerceValueCallback)
+            :base(propertyChangedCallback)
+        {
+            this.CoerceValueCallback = coerceValueCallback;
+        }
+
+        public GenericFrameworkPropertyMetadata(TProperty defaultValue)
+            :base(defaultValue)
+        {
+            
+        }
+
+        public GenericFrameworkPropertyMetadata(
+            TProperty defaultValue,
+            FrameworkPropertyMetadataOptions flags)
+            :base(defaultValue)
+        {
+            this.Flags = flags;
+        }
+
         public override PropertyMetadata ToNonGeneric()
         {
             return new FrameworkPropertyMetadata(
--- GenericDependencyProperties/GenericMetadata/GenericUIPropertyMetadata.cs
+++ GenericDependencyProperties/GenericMetadata/GenericUIPropertyMetadata.cs
@@ -2,12 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using GenericDependencyProperties.GenericCallback;
 
 namespace GenericDependencyProperties.GenericMetadata
 {
     public class GenericUIPropertyMetadata<TProperty, TOwner> : GenericPropertyMetadata<TProperty,TOwner> 
         where TOwner : UIElement
     {
+        public GenericUIPropertyMetadata()
+            :base()
+        {
+            
+        }
+
+        public GenericUIPropertyMetadata(
+            GenericPropertyChangedCallback<TOwner, TProperty> propertyChangedCallback)
+            : base(propertyChangedCallback)
+        {
+
+        }
+
+        public GenericUIPropertyMetadata(TProperty defaultValue)
+            :base(defaultValue)
+        {
+            
+        }
+
+        public GenericUIPropertyMetadata(
+            TProperty defaultValue,
+            GenericPropertyChangedCallback<TOwner, TProperty>  propertyChangedCallback)
+            :base(defaultValue, propertyChangedCallback)
+        {
+            
+        }
+
+        public GenericUIPropertyMetadata(
+            TProperty defaultValue,
+            GenericPropertyChangedCallback<TOwner, TProperty> propertyChangedCallback,
+            GenericCoerceValueCallback<TOwner, TProperty> coerceValueCallback)
+            :base(defaultValue, propertyChangedCallback, coerceValueCallback)
+        {
+        }
+
         public bool IsAnimationProhibited { get; set; }
 
         public override PropertyMetadata ToNonGeneric()
