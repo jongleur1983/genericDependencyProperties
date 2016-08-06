03f7e49ddcdf39876d7bc9fe853c411ad89e39f5
Peter Wendorff <peter.wendorff@gmx.de>
2016-08-06 11:21:05 +0200

    #6: Implement generic RegisterAttachedReadOnly

--- GenericDependencyProperties/GenericDependencyProperty.RegisterAttachedReadOnly.cs
+++ GenericDependencyProperties/GenericDependencyProperty.RegisterAttachedReadOnly.cs
@@ -1,14 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 using System.Windows;
+using GenericDependencyProperties.GenericCallback;
+using GenericDependencyProperties.GenericMetadata;
 
 namespace GenericDependencyProperties
 {
     public partial class GenericDependencyProperty
     {
-        // TODO #6 implement generic RegisterAttachedReadOnly() overloads
+        public static DependencyPropertyKey RegisterAttachedReadOnly<TOwner, TProperty>(
+            string name,
+            GenericPropertyMetadata<TProperty, TOwner> metadata)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterAttachedReadOnly(
+                name,
+                typeof(TProperty),
+                typeof(TOwner),
+                metadata.ToNonGeneric());
+        }
+
+        public static DependencyPropertyKey RegisterAttachedReadOnly<TOwner, TProperty>(
+            Expression<Func<TOwner, TProperty>> memberExpression,
+            GenericPropertyMetadata<TProperty, TOwner> metadata)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterAttachedReadOnly(
+                GetNameFromExpression(memberExpression),
+                typeof(TProperty),
+                typeof(TOwner),
+                metadata.ToNonGeneric());
+        }
+
+
+        // DependencyProperty.RegisterAttachedReadOnly(stringName, propertyType, ownerType, propertyMetadata, validateValueCallback)
+        public static DependencyPropertyKey RegisterAttachedReadOnly<TOwner, TProperty>(
+            string name,
+            GenericPropertyMetadata<TProperty, TOwner> metadata,
+            ValidateValueCallback<TProperty> validateValueCallback)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterAttachedReadOnly(
+                name,
+                typeof(TProperty),
+                typeof(TOwner),
+                metadata.ToNonGeneric(),
+                GetBoxedValidateValueCallback(validateValueCallback));
+        }
+
+        public static DependencyPropertyKey RegisterAttachedReadOnly<TOwner, TProperty>(
+            Expression<Func<TOwner, TProperty>> memberExpression,
+            GenericPropertyMetadata<TProperty, TOwner> metadata,
+            ValidateValueCallback<TProperty> validateValueCallback)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterAttachedReadOnly(
+                GetNameFromExpression(memberExpression),
+                typeof(TProperty),
+                typeof(TOwner),
+                metadata.ToNonGeneric(),
+                GetBoxedValidateValueCallback(validateValueCallback));
+        }
     }
 }
