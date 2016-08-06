1547f7ced93ae8f6addb460b6b68d219a5dd25fd
Peter Wendorff <peter.wendorff@gmx.de>
2016-08-06 10:13:51 +0200

    #5: implement generic variants of RegisterAttached

--- GenericDependencyProperties/GenericDependencyProperty.RegisterAttached.cs
+++ GenericDependencyProperties/GenericDependencyProperty.RegisterAttached.cs
@@ -1,13 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Windows;
+using GenericDependencyProperties.GenericCallback;
+using GenericDependencyProperties.GenericMetadata;
 
 namespace GenericDependencyProperties
 {
     public partial class GenericDependencyProperty
     {
-        // TODO #5 implement generic RegisterAttached() overloads
+        //DependencyProperty.RegisterAttached(name, typeof(string), typeof(GenericDependencyProperty));
+        public static DependencyProperty RegisterAttached<TProperty, TOwner>(string name)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterAttached(
+                name,
+                typeof(TProperty),
+                typeof(TOwner));
+        }
+
+        // DependencyProperty.RegisterAttached(name, typeof(TProperty), typeof(TOwner), propertyMetadata);
+        public static DependencyProperty RegisterAttached<TProperty, TOwner>(
+            string name,
+            GenericPropertyMetadata<TProperty, TOwner> propertyMetadata)
+            where TOwner : DependencyObject
+
+        {
+            return DependencyProperty.RegisterAttached(
+                name,
+                typeof(TProperty),
+                typeof(TOwner),
+                propertyMetadata.ToNonGeneric());
+        }
+
+        // DependencyProperty.RegisterAttached(name, typeof(TProperty), typeof(TOwner), PropertyMetadata, validateValueCallback);
+        public static DependencyProperty RegisterAttached<TProperty, TOwner> (
+            string name,
+            GenericPropertyMetadata<TProperty, TOwner> propertyMetadata,
+            ValidateValueCallback<TProperty> validateValueCallback)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterAttached(
+                name,
+                typeof(TProperty),
+                typeof(TOwner),
+                propertyMetadata.ToNonGeneric(),
+                GetBoxedValidateValueCallback(validateValueCallback));
+        }
+
+        //DependencyProperty.RegisterAttached(name, typeof(string), typeof(GenericDependencyProperty));
+        public static DependencyProperty RegisterAttached<TProperty, TOwner>(
+            Expression<Func<TOwner, TProperty>> memberExpression)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterAttached(
+                GetNameFromExpression(memberExpression),
+                typeof(TProperty),
+                typeof(TOwner));
+        }
+
+        // DependencyProperty.RegisterAttached(name, typeof(TProperty), typeof(TOwner), propertyMetadata);
+        public static DependencyProperty RegisterAttached<TProperty, TOwner>(
+            Expression<Func<TOwner, TProperty>> memberExpression,
+            GenericPropertyMetadata<TProperty, TOwner> propertyMetadata)
+            where TOwner : DependencyObject
+
+        {
+            return DependencyProperty.RegisterAttached(
+                GetNameFromExpression(memberExpression),
+                typeof(TProperty),
+                typeof(TOwner),
+                propertyMetadata.ToNonGeneric());
+        }
+
+        // DependencyProperty.RegisterAttached(name, typeof(TProperty), typeof(TOwner), PropertyMetadata, validateValueCallback);
+        public static DependencyProperty RegisterAttached<TProperty, TOwner>(
+            Expression<Func<TOwner, TProperty>> memberExpression,
+            GenericPropertyMetadata<TProperty, TOwner> propertyMetadata,
+            ValidateValueCallback<TProperty> validateValueCallback)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterAttached(
+                GetNameFromExpression(memberExpression),
+                typeof(TProperty),
+                typeof(TOwner),
+                propertyMetadata.ToNonGeneric(),
+                GetBoxedValidateValueCallback(validateValueCallback));
+        }
     }
 }
