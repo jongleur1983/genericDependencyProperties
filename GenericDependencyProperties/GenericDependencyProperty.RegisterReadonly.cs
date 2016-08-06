d9ade396418ef89f8fd19219f20a467c35de80bd
Peter Wendorff <peter.wendorff@gmx.de>
2016-08-06 10:35:56 +0200

    #7: implement generic variants of RegisterReadOnly

--- GenericDependencyProperties/GenericDependencyProperty.RegisterReadonly.cs
+++ GenericDependencyProperties/GenericDependencyProperty.RegisterReadonly.cs
@@ -1,13 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using GenericDependencyProperties.GenericCallback;
+using GenericDependencyProperties.GenericMetadata;
 
 namespace GenericDependencyProperties
 {
     public partial class GenericDependencyProperty
     {
-        // TODO #7 implement generic RegisterReadOnly() overloads
+        // DependencyProperty.RegisterReadOnly(stringName, typePropertyType, typeOwnerType, propertyMetadata);
+        public static DependencyPropertyKey RegisterReadOnly<TProperty, TOwner>(
+            string name,
+            GenericPropertyMetadata<TProperty, TOwner> propertyMetadata)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterReadOnly(
+                name,
+                typeof(TProperty),
+                typeof(TOwner),
+                propertyMetadata.ToNonGeneric());
+        }
+        
+        // DependencyProperty.RegisterReadOnly(stringName, typePropertyType, typeOwnerType, propertyMetadata, validateValueCallback)
+        public static DependencyPropertyKey RegisterReadOnly<TProperty, TOwner>(
+            string name, 
+            GenericPropertyMetadata<TProperty, TOwner> propertyMetadata,
+            ValidateValueCallback<TProperty> validateValueCallback)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterReadOnly(
+                name,
+                typeof(TProperty),
+                typeof(TOwner),
+                propertyMetadata.ToNonGeneric(),
+                GetBoxedValidateValueCallback(validateValueCallback));
+        }
+
+        // DependencyProperty.RegisterReadOnly(stringName, typePropertyType, typeOwnerType, propertyMetadata);
+        public static DependencyPropertyKey RegisterReadOnly<TProperty, TOwner>(
+            Expression<Func<TOwner, TProperty>> memberExpression,
+            GenericPropertyMetadata<TProperty, TOwner> propertyMetadata)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterReadOnly(
+                GetNameFromExpression(memberExpression),
+                typeof(TProperty),
+                typeof(TOwner),
+                propertyMetadata.ToNonGeneric());
+        }
+
+        // DependencyProperty.RegisterReadOnly(stringName, typePropertyType, typeOwnerType, propertyMetadata, validateValueCallback)
+        public static DependencyPropertyKey RegisterReadOnly<TProperty, TOwner>(
+            Expression<Func<TOwner, TProperty>> memberExpression,
+            GenericPropertyMetadata<TProperty, TOwner> propertyMetadata,
+            ValidateValueCallback<TProperty> validateValueCallback)
+            where TOwner : DependencyObject
+        {
+            return DependencyProperty.RegisterReadOnly(
+                GetNameFromExpression(memberExpression),
+                typeof(TProperty),
+                typeof(TOwner),
+                propertyMetadata.ToNonGeneric(),
+                GetBoxedValidateValueCallback(validateValueCallback));
+        }
+
     }
 }
