72733697056c8e33d1ef740a9356ce5e33f90149
Peter Wendorff <peter.wendorff@gmx.de>
2016-08-06 12:17:25 +0200

    #13: implement generic DependencyProperty.AddOwner()

---
+++ GenericDependencyProperties/GenericDependencyProperty.AddOwner.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using GenericDependencyProperties.GenericMetadata;
+
+namespace GenericDependencyProperties
+{
+    public static partial class GenericDependencyProperty 
+    {
+        public static DependencyProperty AddOwner<TOwner>(this DependencyProperty original)
+            where TOwner : DependencyObject
+        {
+            return original.AddOwner(typeof(TOwner));
+        }
+
+        public static DependencyProperty AddOwner<TOwner, TProperty>(
+            this DependencyProperty original,
+            GenericPropertyMetadata<TProperty, TOwner> metadata)
+            where TOwner : DependencyObject
+        {
+            return original.AddOwner(typeof(TOwner), metadata.ToNonGeneric());
+        }
+    }
+}
