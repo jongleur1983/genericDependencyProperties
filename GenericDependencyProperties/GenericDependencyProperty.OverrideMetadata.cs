c2cf9ab757b5e166ff4438a36d395a7f2e8cb9f2
Peter Wendorff <peter.wendorff@gmx.de>
2016-08-06 18:38:54 +0200

    #14: add generic overload for PropertyMetadata.OverrideMetadata. Added as an extension method, that way it can be used on existing DependencyProperties as well.

---
+++ GenericDependencyProperties/GenericDependencyProperty.OverrideMetadata.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using GenericDependencyProperties.GenericMetadata;
+
+namespace GenericDependencyProperties
+{
+    public static partial class GenericDependencyProperty
+    {
+        public static void OverrideMetadata<TOwner, TProperty>(
+            this DependencyProperty dp,
+            GenericPropertyMetadata<TProperty, TOwner> metadata)
+            where TOwner : DependencyObject
+        {
+            dp.OverrideMetadata(
+                typeof(TOwner),
+                metadata.ToNonGeneric());
+        }
+
+        public static void OverrideMetadata<TOwner, TProperty>(
+            this DependencyProperty dp,
+            GenericPropertyMetadata<TProperty, TOwner> metadata,
+            DependencyPropertyKey dependencyPropertyKey)
+            where TOwner: DependencyObject
+        {
+            dp.OverrideMetadata(
+                typeof(TOwner),
+                metadata.ToNonGeneric(), 
+                dependencyPropertyKey);
+        }
+    }
+}
