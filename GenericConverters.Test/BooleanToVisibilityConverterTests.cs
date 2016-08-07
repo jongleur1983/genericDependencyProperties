48a36f3cbcb93140564db9dfc960e97be1edadb9
Peter Wendorff <peter.wendorff@gmx.de>
2016-08-07 13:10:24 +0200

    #10: implement a generic BooleanToVisibility, including unit tests.

---
+++ GenericConverters.Test/BooleanToVisibilityConverterTests.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using GenericConverters.ImprovedStandardConverters;
+using Xunit;
+
+namespace GenericConverters.Test
+{
+    public class BooleanToVisibilityConverterTests
+    {
+        [Theory]
+        [InlineData(true, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedHidden, Visibility.Collapsed)]
+        [InlineData(true, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedVisible, Visibility.Collapsed)]
+        [InlineData(true, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenCollapsed, Visibility.Hidden)]
+        [InlineData(true, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenVisible, Visibility.Hidden)]
+        [InlineData(true, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleCollapsed, Visibility.Visible)]
+        [InlineData(true, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleHidden, Visibility.Visible)]
+
+        [InlineData(false, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedHidden, Visibility.Hidden)]
+        [InlineData(false, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedVisible, Visibility.Visible)]
+        [InlineData(false, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenCollapsed, Visibility.Collapsed)]
+        [InlineData(false, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenVisible, Visibility.Visible)]
+        [InlineData(false, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleCollapsed, Visibility.Collapsed)]
+        [InlineData(false, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleHidden, Visibility.Hidden)]
+        public void ConvertsCorrect(
+            bool input,
+            BooleanToVisibilityConverter.BooleanToVisibilityMapping parameter,
+            Visibility expectedResult)
+        {
+            var converter = new BooleanToVisibilityConverter();
+            var result = converter.Convert(input, parameter, CultureInfo.InvariantCulture);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(Visibility.Visible, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleHidden, true)]
+        [InlineData(Visibility.Visible, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleCollapsed, true)]
+        [InlineData(Visibility.Visible, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedVisible, false)]
+        [InlineData(Visibility.Visible, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenVisible, false)]
+        [InlineData(Visibility.Collapsed, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedHidden, true)]
+        [InlineData(Visibility.Collapsed, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedVisible, true)]
+        [InlineData(Visibility.Collapsed, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenCollapsed, false)]
+        [InlineData(Visibility.Collapsed, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleCollapsed, false)]
+        [InlineData(Visibility.Hidden, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenCollapsed, true)]
+        [InlineData(Visibility.Hidden, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenVisible, true)]
+        [InlineData(Visibility.Hidden, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedHidden, false)]
+        [InlineData(Visibility.Hidden, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleHidden, false)]
+        
+        public void ConvertsBackCorrect(
+            Visibility input,
+            BooleanToVisibilityConverter.BooleanToVisibilityMapping parameter,
+            bool expectedResult)
+        {
+            var converter = new BooleanToVisibilityConverter();
+            var result = converter.ConvertBack(input, parameter, CultureInfo.InvariantCulture);
+
+            Assert.Equal(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData(Visibility.Visible, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedHidden)]
+        [InlineData(Visibility.Visible, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenCollapsed)]
+        [InlineData(Visibility.Collapsed, BooleanToVisibilityConverter.BooleanToVisibilityMapping.HiddenVisible)]
+        [InlineData(Visibility.Collapsed, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleHidden)]
+        [InlineData(Visibility.Hidden, BooleanToVisibilityConverter.BooleanToVisibilityMapping.VisibleCollapsed)]
+        [InlineData(Visibility.Hidden, BooleanToVisibilityConverter.BooleanToVisibilityMapping.CollapsedVisible)]
+        public void ContertBackThrowsCorrect(
+            Visibility input,
+            BooleanToVisibilityConverter.BooleanToVisibilityMapping parameter)
+        {
+            var converter = new BooleanToVisibilityConverter();
+            Assert.Throws<ArgumentOutOfRangeException>(
+                () => converter.ConvertBack(
+                    input,
+                    parameter,
+                    CultureInfo.InvariantCulture));
+        }
+    }
+}
--- GenericConverters/ImprovedStandardConverters/BooleanToVisibilityConverter.cs
+++ GenericConverters/ImprovedStandardConverters/BooleanToVisibilityConverter.cs
@@ -1,20 +1,142 @@
-﻿using System.Globalization;
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
 using System.Windows;
 
 namespace GenericConverters.ImprovedStandardConverters
 {
-    public class BooleanToVisibilityConverter : GenericValueConverter<bool?, Visibility>
+    public class BooleanToVisibilityConverter : GenericValueConverter<bool, Visibility, BooleanToVisibilityConverter.BooleanToVisibilityMapping>
     {
-        public override Visibility Convert(bool? from, CultureInfo culture)
+        public enum BooleanToVisibilityMapping : byte
         {
-            return from == true
-                ? Visibility.Visible
-                : Visibility.Hidden;
+            // decoded bitwise to:
+            // - true is visible
+            // - true is collapsed
+            // - true is hidden
+            // - false is visible
+            // - false is collapsed
+            // - false is hidden
+            VisibleHidden    = (1 << 0) | (0 << 1) | (0 << 2) | (0 << 3) | (0 << 4) | (1 << 5),
+            VisibleCollapsed = (1 << 0) | (0 << 1) | (0 << 2) | (0 << 3) | (1 << 4) | (0 << 5),
+            HiddenVisible    = (0 << 0) | (0 << 1) | (1 << 2) | (1 << 3) | (0 << 4) | (0 << 5),
+            HiddenCollapsed  = (0 << 0) | (0 << 1) | (1 << 2) | (0 << 3) | (1 << 4) | (0 << 5),
+            CollapsedVisible = (0 << 0) | (1 << 1) | (0 << 2) | (1 << 3) | (0 << 4) | (0 << 5),
+            CollapsedHidden  = (0 << 0) | (1 << 1) | (0 << 2) | (0 << 3) | (0 << 4) | (1 << 5)
         }
 
-        public override bool? ConvertBack(Visibility value, CultureInfo culture)
+        public override Visibility Convert(bool @from, BooleanToVisibilityMapping parameter, CultureInfo culture)
         {
-            return value == Visibility.Visible;
+            if (@from)
+            {
+                var trueEncoding = (byte) parameter & 7; //((1 << 0) | (1 << 1) | (1 << 2));
+                if (trueEncoding == 1)
+                {
+                    return Visibility.Visible;
+                } 
+                else if (trueEncoding == 2)
+                {
+                    return Visibility.Collapsed;
+                }
+                else if (trueEncoding == 4)
+                {
+                    return Visibility.Hidden;
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException(nameof(parameter), (int)parameter, typeof(BooleanToVisibilityMapping));
+                }
+            }
+            else
+            {
+                var falseEncoding = (byte) parameter & 56; //((1 << 3) | (1 << 4) | (1 << 5));
+                if (falseEncoding == 8)
+                {
+                    return Visibility.Visible;
+                }
+                else if (falseEncoding == 16)
+                {
+                    return Visibility.Collapsed;
+                }
+                else if (falseEncoding == 32)
+                {
+                    return Visibility.Hidden;
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException(nameof(parameter), (int)parameter, typeof(BooleanToVisibilityMapping));
+                }
+            }
+        }
+
+        public override bool ConvertBack(Visibility value, BooleanToVisibilityMapping parameter, CultureInfo culture)
+        {
+            if (value == Visibility.Visible)
+            {
+                if (parameter == BooleanToVisibilityMapping.VisibleCollapsed
+                    || parameter == BooleanToVisibilityMapping.VisibleHidden)
+                {
+                    return true;
+                }
+                else if (parameter == BooleanToVisibilityMapping.CollapsedVisible
+                         || parameter == BooleanToVisibilityMapping.HiddenVisible)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(value),
+                        value,
+                        $"With parameter={parameter} the value {value} has no valid mapping");
+                }
+            }
+            else if (value == Visibility.Hidden)
+            {
+                if (parameter == BooleanToVisibilityMapping.HiddenCollapsed
+                    || parameter == BooleanToVisibilityMapping.HiddenVisible)
+                {
+                    return true;
+                }
+                else if (parameter == BooleanToVisibilityMapping.CollapsedHidden
+                         || parameter == BooleanToVisibilityMapping.VisibleHidden)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(value),
+                        value,
+                        $"With parameter={parameter} the value {value} has no valid mapping");
+                }
+            }
+            else if (value == Visibility.Collapsed)
+            {
+                if (parameter == BooleanToVisibilityMapping.CollapsedHidden
+                    || parameter == BooleanToVisibilityMapping.CollapsedVisible)
+                {
+                    return true;
+                }
+                else if (parameter == BooleanToVisibilityMapping.HiddenCollapsed
+                         || parameter == BooleanToVisibilityMapping.VisibleCollapsed)
+                {
+                    return false;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(value),
+                        value,
+                        $"With parameter={parameter} the value {value} has no valid mapping");
+                }
+            }
+            else
+            {
+                throw new InvalidEnumArgumentException(
+                    nameof(value),
+                    (int)value,
+                    typeof(Visibility));
+            }
         }
     }
 }
