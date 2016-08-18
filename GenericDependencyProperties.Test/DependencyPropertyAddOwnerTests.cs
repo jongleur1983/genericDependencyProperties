fa129723940fa1a71b9aa4c02589dc2e0a69b5a3
Peter Wendorff <peter.wendorff@gmx.de>
2016-08-19 00:40:27 +0200

    start writing tests for the DependencyProperties project

---
+++ GenericDependencyProperties.Test/DependencyPropertyAddOwnerTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using Xunit;
+using GenericDependencyProperties.GenericMetadata;
+
+namespace GenericDependencyProperties.Test
+{
+    public class AddOwnerTests
+    {
+        [Fact]
+        public void AddADependencyPropertyToSecondDependencyObjectWithDifferentDefaultValue()
+        {
+            var defaultValue = 5d;
+            FirstDepenencyObject.dp.AddOwner<SecondDependencyObject, double>(
+                new GenericPropertyMetadata<double, SecondDependencyObject>(defaultValue));
+
+            var sdo = new SecondDependencyObject();
+
+            Assert.Equal(defaultValue, sdo.GetValue(FirstDepenencyObject.dp));
+        }
+
+        [Fact]
+        public void AddADependencyPropertyToSecondDependencyObjectWithSameDefaultValue()
+        {
+            var defaultValue = 5d;
+            FirstDepenencyObject.dp.AddOwner<ThirdDependencyObject>();
+
+            var sdo = new ThirdDependencyObject();
+
+            Assert.Equal(
+                FirstDepenencyObject.dp.DefaultMetadata.DefaultValue,
+                sdo.GetValue(FirstDepenencyObject.dp));
+        }
+
+        public class FirstDepenencyObject : DependencyObject
+        {
+            public static readonly DependencyProperty dp = DependencyProperty.Register(
+                "Test",
+                typeof(double),
+                typeof(FirstDepenencyObject),
+                new PropertyMetadata(20d));
+        }
+
+        public class SecondDependencyObject : DependencyObject
+        {
+        }
+
+        public class ThirdDependencyObject : DependencyObject
+        {
+        }
+    }
+}
---
+++ GenericDependencyProperties.Test/DependencyPropertyPropertyChangedCallbackTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using GenericDependencyProperties.GenericMetadata;
+using Xunit;
+
+namespace GenericDependencyProperties.Test
+{
+    public class DependencyPropertyPropertyChangedCallbackTests
+    {
+        [Fact]
+        public void PropertyChancedCallbackIsCalled()
+        {
+            var objectUnderTest = new FirstDependencyObject();
+            Assert.Equal(
+                FirstDependencyObject.DefaultTestValue,
+                objectUnderTest.Test);
+
+            objectUnderTest.Test = 42;
+            Assert.Equal(
+                42,
+                objectUnderTest.Test);
+            Assert.Equal(
+                FirstDependencyObject.DefaultTestValue,
+                objectUnderTest.LastValue);
+        }
+
+        public class FirstDependencyObject : DependencyObject
+        {
+            public const int DefaultTestValue = 5;
+
+            public static readonly DependencyProperty dp = GenericDependencyProperty
+                .Register<int, FirstDependencyObject>(
+                    fdo => fdo.Test,
+                    new GenericPropertyMetadata<int, FirstDependencyObject>(
+                        DefaultTestValue,
+                        Callback));
+
+            private static void Callback(FirstDependencyObject d, DependencyPropertyChangedEventArgs<int> e)
+            {
+                d.LastValue = e.OldValue;
+            }
+
+            public int LastValue
+            {
+                get;
+                set;
+            }
+
+            public int Test
+            {
+                get { return (int)this.GetValue(dp); }
+                set { this.SetValue(dp, value); }
+            }
+        }
+    }
+}
